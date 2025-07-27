6e83e278a9164e6a698e7fbd0c6f6d4fbe2a3bfa
mvfsilva.develcode <viniciusfranca09@hotmail.com>
2025-07-27 09:48:26 -0300

    feat: token logic added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
+import tailwindConfig from './tailwind.config';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(tailwindConfig),
+    dts({
+      insertTypesEntry: true,
+    }),
+    federation({
+      name: "teddy_design_system",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Theme": "./src/ThemeProvider",
+        "./tokens": "./src/themes/index",
+      },
+      shared: ["react", "react-dom", "styled-components"],
+    }),
+  ],
+
+  server: {
+    port: 3000,
+  },
+
+  build: {
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+    lib: {
+      // Aqui é onde especificamos o ponto de entrada
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "TeddyDesignSystem",
+      formats: ["es", "umd"],
+      fileName: (format) => `teddy-design-system.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "styled-components"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "styled-components": "styled",
+        },
+      },
+    },
+  },
+});
