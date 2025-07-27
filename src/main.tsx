cfa101d592f0bf0d47f822cdacdc13ce6b7a3e1a
mvfsilva.develcode <viniciusfranca09@hotmail.com>
2025-07-27 10:49:28 -0300

    fix: adjusting main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,8 @@
 import "./styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "./hooks/ThemeProvider";
-import { Button } from "./components/Button";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <h1>Teddy Design System</h1>
-      <Button/>
-    </ThemeProvider>
   </React.StrictMode>
 );
