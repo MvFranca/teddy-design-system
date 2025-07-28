505a9a48bb8513659cac42a6ae0b37ac0e1d66d6
mvfsilva.develcode <viniciusfranca09@hotmail.com>
2025-07-28 16:55:46 -0300

    feat: add input again

--- src/themes/tokens/colors.js
+++
@@ -1,9 +0,0 @@
-export const colors = {
-    primaryMain: 'rgba(236, 103, 36, 1)',
-    primaryContrast: '#FFFFFF',
-    primaryBorder: 'rgba(236, 103, 36, 1)',
-    neutralText: '#333333',
-    disabledOpacity: 0.5,
-    borderDefault: 'rgba(217, 217, 217, 1)',
-    textPrimary: '#000000',
-};
--- src/themes/tokens/typography.js
+++
@@ -1,5 +0,0 @@
-export const typography = {
-    fontFamily: '"Inter", "Segoe UI", Tahoma, Geneva, Verdana, sans-serif',
-    fontWeightMedium: 500,
-    fontWeightBold: 700,
-};
